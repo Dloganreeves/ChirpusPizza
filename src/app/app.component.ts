9d8d38274168ddf655507e7064d1b3dcaa4e7f93
Dloganreeves <loganreeves1@yahoo.com>
2024-03-07 14:43:46 -0500

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MenuItem} from './models/menu-item'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ChurpusPizza';
+  title = 'Churpus Pizza';
+
+menu: MenuItem[] = [
+  {
+    name:"Cheese Pizza",
+    category: "Pizza",
+    price: 12,
+    vegetarian: true,
+    soldOut: false
+
+  },
+  {
+    name:"Hot Honey Chicken Pizza",
+    category: "Pizza",
+    price: 14.50,
+    vegetarian: false,
+    soldOut: true
+  },
+  {
+    name:"Bone-In Wings",
+    category: "Wings",
+    price: 13,
+    vegetarian: false,
+    soldOut: true
+  },
+    
+  {
+    name:"Cauliflower Wings",
+    category: "Wings",
+    price: 10,
+    vegetarian: true,
+    soldOut: false
+  }
+]
+
 }
+
+
+
+
